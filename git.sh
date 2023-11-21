9f1b4639635b2e6d934ed4aef42ca2f521b8a912
Kashaboina <gouthami.kashaboina@capgemini.com>
2023-11-21 17:22:06 +0530

    Session Update

---
+++ git.sh
@@ -0,0 +1,15 @@
+sleep 1
+git init
+sleep 1
+git add .
+sleep 1
+git status
+sleep 1
+git commit -m "Session Update"
+sleep 1
+git push -f
+sleep 1
+echo ""
+echo -n "Successfully Updated to Git!"
+echo ""
+sleep 1
